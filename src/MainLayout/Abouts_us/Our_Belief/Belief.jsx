cd93527644ba759593b96e47e041b5e3996bf96c
SahilPawar10 <pawarsp959@gmail.com>
2024-03-02 20:53:20 +0530

    ourBelief_and_OurTeam_Done

--- src/MainLayout/Abouts_us/Our_Belief/Belief.jsx
+++ src/MainLayout/Abouts_us/Our_Belief/Belief.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import withLayout from "../..";
+import "./belief.css";
 import Banner from "../../Components/Banner";
 import beliefBanner from "../../../assets/our_beliefBanner.png";
 import beliefBanner_mob from "../../../assets/our_beliefBannermob.png";
+import grid1 from "../../../assets/We-believe-in-children-150x150.png";
+import grid2 from "../../../assets/All-human-beings.png";
+import grid3 from "../../../assets/We-are-in-solidarity-.png";
+import grid4 from "../../../assets/grid-4.png";
+import grid5 from "../../../assets/We-believe-in-collaboration.png";
+import grid6 from "../../../assets/We-believe-in-children-150x150.png";
+import grid7 from "../../../assets/We-believe-in-giving-each-other-the-.png";
+import grid8 from "../../../assets/We-believe-that-all-of-us-are-a.png";
 
 function Belief() {
   return (
@@ -13,6 +22,116 @@ function Belief() {
         submenu1={"About Us"}
         submenu2={"Our Belief "}
       />
+      <div className="belief-container">
+        <div className="belief-heading contenr-1200">
+          <h2>
+            Injustice anywhere is a threat to justice everywhere.” - Martin
+            Luther King Jr.
+          </h2>
+        </div>
+        <div className="belief-content contenr-1200">
+          <h2>Our Belief</h2>
+          <div className="beliefs-grid">
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid1} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  We believe in children. We believe in their right to be heard
+                  and participate in decisions that affect them.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------2--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid2} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  All human beings on our planet regardless of geography,
+                  religious orientation, skin color, gender, or sexual
+                  orientation, are of equal value.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------3--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid3} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  We are in solidarity with peaceful movements to prevent the
+                  marginalization of human beings everywhere.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------4--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid4} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  All children, regardless of race, religion, sect, or caste
+                  have the same fundamental rights.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------5--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid5} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  We believe in collaboration, inclusion, partnerships, ordinary
+                  heroes, honesty, agility, straight talk, and miracles.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------6--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid6} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  We believe in giving each other the benefit of the doubt,
+                  leveraging each other’s best skills, listening, learning, and
+                  caring deeply about all stakeholders.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------7--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid7} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  All children have the right to grow up in a stable, loving,
+                  and nurturing environment.
+                </p>
+              </div>
+            </div>
+            {/*---------------------------8--------------------- */}
+            <div className="beliefs-points">
+              <div className="beliefs-img">
+                <img src={grid8} alt="" />
+              </div>
+              <div className="beliefs-desc">
+                <p>
+                  We believe that all of us are a stakeholder in the care of the
+                  planet and each other the world over.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/MainLayout/Abouts_us/Our_Team/Team.jsx
+++ src/MainLayout/Abouts_us/Our_Team/Team.jsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../../Components/Banner";
+import "./team.css";
 import teamPhoto from "../../../assets/rsz_2bhajani_mandal1.jpg";
 import teamPhoto_mob from "../../../assets/bhajani_mandal1_mob.jpg";
+import member1 from "../../../assets/member2.jpeg";
+import member2 from "../../../assets/member3.jpeg";
+import member3 from "../../../assets/member4.jpeg";
+import member4 from "../../../assets/member5.jpeg";
+import member5 from "../../../assets/member6.jpeg";
+import member6 from "../../../assets/member7.jpeg";
+
 import withLayout from "../..";
 
+const teamData = [
+  {
+    id: "1",
+    image: member1,
+    name: "Vikas Pawar",
+    role: "Chairman",
+  },
+  {
+    id: "2",
+    image: member3,
+    name: "Dipak Pawar",
+    role: "Director",
+  },
+  {
+    id: "3",
+    image: member5,
+    name: "Sahil Pawar",
+    role: "Director",
+  },
+  {
+    id: "4",
+    image: member6,
+    name: "Ajit Kadamr",
+    role: "Director",
+  },
+  {
+    id: "5",
+    image: member4,
+    name: "Rupesh Kadam ",
+    role: "Director",
+  },
+  {
+    id: "6",
+    image: member2,
+    name: "Vishal Nikam",
+    role: "Director",
+  },
+];
+
 function Team() {
+  const [Active, setActive] = useState(false);
+
+  const handleTeam = () => {
+    setActive(!Active);
+    console.log(Active, "teamActive");
+  };
   return (
     <div>
       <Banner
@@ -13,9 +66,106 @@ function Team() {
         submenu1={"About Us"}
         submenu2={"Our Team "}
       />
+      <div className="team-container ">
+        <div className="team-nav padd-28">
+          <ul>
+            <li
+              className={!Active ? "active-team" : "notActive"}
+              onClick={handleTeam}
+            >
+              Board Members
+            </li>
+            <li
+              className={Active ? "active-team" : "notActive"}
+              onClick={handleTeam}
+            >
+              Management Members
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* ........................Team Types....................... */}
+      {Active ? Management() : Board()}
     </div>
   );
 }
 
-// export default Team;
+function Board() {
+  return (
+    <div>
+      <div className="team-type  ">
+        <div className="team-desc ">
+          <p className="padd-28">
+            The Board consists of a group of highly accomplished professionals
+            from public, private and development sector who share a common
+            passion for empowering women and girls to alleviate poverty and
+            reduce social exclusion. The board chaired by Mr. Mathew Cherian
+            provides governance and leadership for enabling Swarajya India
+            accomplish its mission. The board is actively engaged in governing
+            Swarajya as a lead development institution in the country working in
+            close collaboration with other development partners.
+          </p>
+        </div>
+        <div className="team-members">
+          <div className="teamtype-container contenr-1200 ">
+            <h2>Board Members</h2>
+            <div className="team-grid">
+              {teamData.map((item) => {
+                return (
+                  <div className="team-member">
+                    <div className="member-photo">
+                      <img src={item.image} alt="" />
+                    </div>
+                    <div className="member-info">
+                      <h4 className="memberName">{item.name}</h4>
+                      <p className="memberRole">{item.role}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Management() {
+  return (
+    <div>
+      <div className="team-type ">
+        <div className="team-desc ">
+          <p className="padd-28">
+            CARE India has a leadership team which is a blend of professionals
+            from the private and development sector. They bring a wide range of
+            experience to bear upon development challenges and create innovative
+            solutions for long term sustainable impact on the ground.
+          </p>
+        </div>
+        <div className="team-members">
+          <div className="teamtype-container contenr-1200 ">
+            <h2>Management Team</h2>
+            <div className="team-grid">
+              {teamData.map((item) => {
+                return (
+                  <div className="team-member">
+                    <div className="member-photo">
+                      <img src={item.image} alt="" />
+                    </div>
+                    <div className="member-info">
+                      <h4 className="memberName">{item.name}</h4>
+                      <p className="memberRole">{item.role}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default withLayout(Team);
