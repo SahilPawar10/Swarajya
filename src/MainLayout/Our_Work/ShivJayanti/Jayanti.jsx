0fe45abfd10cf4a702dd71ef413a930dbc488bb6
SahilPawar10 <pawarsp959@gmail.com>
2024-03-03 18:22:02 +0530

    JayantiDay2_GalleryDone

--- src/MainLayout/Our_Work/ShivJayanti/Jayanti.jsx
+++ src/MainLayout/Our_Work/ShivJayanti/Jayanti.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./jayanti.css";
 import Banner from "../../Components/Banner";
 import withLayout from "../..";
 import jayantiImage from "../../../assets/jayantiBanner.jpeg";
 import shobhaYatra from "../../../assets/jayantiProg1.jpg";
 import bhashan from "../../../assets/bhashan2.jpg";
+import haldiKunku from "../../../assets/haldi-kunku.jpg";
+import JayatiGal1 from "../../../assets/bhajani_mandal.jpeg";
+import JayantiGal2 from "../../../assets/Education-top-10-blog-banner.jpg";
+import JayantiGal3 from "../../../assets/health-banner.jpg";
+import JayantiGal4 from "../../../assets/involved_bannermob.jpg";
+import JayantiGal5 from "../../../assets/our_beliefBannermob.png";
 import { Link } from "react-router-dom";
 
 function Jayanti() {
+  const scrollContainerRef = useRef(null);
+  // const scrollContainer = scrollContainerRef.current;
+
+  useEffect(() => {
+    let scrollContainer = document.getElementById("jayantiGallery");
+    let backButton = document.getElementById("backBtn");
+    let nextButton = document.getElementById("nextBtn");
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+      evt.preventDefault();
+      scrollContainer.scrollLeft += evt.deltaX;
+    });
+
+    nextButton.addEventListener("click", () => {
+      console.log("nextClicked");
+      scrollContainer.scrollLeft += 190;
+    });
+
+    backButton.addEventListener("click", () => {
+      console.log("backClicked");
+      scrollContainer.scrollLeft -= 190;
+    });
+  }, []);
+
+  // let scrollContainer = document.getElementById("jayantiGallery");
+  // let backButton = document.getElementById("backBtn");
+  // let nextButton = document.getElementById("nextBtn");
+
+  // scrollContainer.addEventListener("wheel", (evt) => {
+  //   evt.preventDefault();
+  //   scrollContainer.scrollLeft += evt.deltaY;
+  // });
+
+  // nextButton.addEventListener("click", () => {
+  //   scrollContainer.scrollLeft += 900;
+  // });
+
   return (
     <div>
       <Banner
@@ -56,13 +99,12 @@ function Jayanti() {
             <div className="jay-pro-content">
               <div className="jay-pro-content-details">
                 <div className="type-name">
-                  <h4>
-                    Kerala Flood <br />
-                    Response
-                  </h4>
+                  <h4>What is ShivJayanti ?</h4>
                   <p>
-                    Rapid aid to the destruction and loss caused by the flooding
-                    and landslides in Idukki and Wayanad.
+                    We organize a procession on Shiv Jayanti where we sing
+                    traditional songs of Shivaji Maharaj and enlighten the
+                    people. This procession takes place throughout the entire
+                    village.
                   </p>
                   <Link className="read-more">
                     <span>Read More</span>
@@ -80,12 +122,14 @@ function Jayanti() {
               <div className="jay-pro-content-details">
                 <div className="type-name">
                   <h4>
-                    Kerala Flood <br />
-                    Response
+                    Speech contests <br />
                   </h4>
                   <p>
-                    Rapid aid to the destruction and loss caused by the flooding
-                    and landslides in Idukki and Wayanad.
+                    We conduct children's speech contests. These competitions
+                    serve as a platform for young minds to develop their
+                    communication skills, critical thinking abilities, and
+                    confidence, empowering them to excel in various aspects of
+                    life
                   </p>
                   <Link className="read-more">
                     <span>Read More</span>
@@ -96,8 +140,8 @@ function Jayanti() {
           </div>
           <div className="jay-pro-type-item">
             <div className="jay-pro-img">
-              <img className="jay-pro" src={shobhaYatra} alt="" />
-              <h4>Procession</h4>
+              <img className="jay-pro" src={haldiKunku} alt="" />
+              <h4>Haldi KumKum</h4>
             </div>
             <div className="jay-pro-content">
               <div className="jay-pro-content-details">
@@ -143,9 +187,28 @@ function Jayanti() {
         </div>
       </div>
       {/* -----------------------------------------Jayanti Gallery-------------------------------------------------- */}
-      <div className="jayanti-Gallery">
+      <div className="jayanti-Gallery contenr-1200">
         <div className="jaya-progr-heading">
           <h2>Gallery</h2>
+        </div>
+        <div className="gallery-slider">
+          <span>
+            <i class="fa fa-arrow-left" aria-hidden="true" id="backBtn"></i>
+          </span>
+          <div className="images" id="jayantiGallery" ref={scrollContainerRef}>
+            <img src={bhashan} alt="" />
+            <img src={haldiKunku} alt="" />
+            <img src={jayantiImage} alt="" />
+            <img src={shobhaYatra} alt="" />
+            <img src={JayatiGal1} alt="" />
+            <img src={JayantiGal2} alt="" />
+            <img src={JayantiGal3} alt="" />
+            <img src={JayantiGal4} alt="" />
+            <img src={JayantiGal5} alt="" />
+          </div>
+          <span>
+            <i class="fa fa-arrow-right" aria-hidden="true" id="nextBtn"></i>
+          </span>
         </div>
       </div>
     </div>
