9007e5887d182453ff4b3c01f78819064d82ba30
SahilPawar10 <pawarsp959@gmail.com>
2024-03-03 12:58:52 +0530

    JayantiDay2

--- src/MainLayout/Our_Work/ShivJayanti/Jayanti.jsx
+++ src/MainLayout/Our_Work/ShivJayanti/Jayanti.jsx
@@ -3,6 +3,9 @@ import "./jayanti.css";
 import Banner from "../../Components/Banner";
 import withLayout from "../..";
 import jayantiImage from "../../../assets/jayantiBanner.jpeg";
+import shobhaYatra from "../../../assets/jayantiProg1.jpg";
+import bhashan from "../../../assets/bhashan2.jpg";
+import { Link } from "react-router-dom";
 
 function Jayanti() {
   return (
@@ -37,6 +40,112 @@ function Jayanti() {
             among the people of Maharashtra and beyond, as they reflect on the
             enduring impact of Shivaji Maharaj's legacy on Indian society.
           </p>
+        </div>
+      </div>
+      {/* -----------------------------------------Jayanti Programs-------------------------------------------------- */}
+      <div className="jayanti-programs contenr-1200">
+        <div className="jaya-progr-heading">
+          <h2>Key Programs</h2>
+        </div>
+        <div className="jayanti-progr-types">
+          <div className="jay-pro-type-item">
+            <div className="jay-pro-img">
+              <img className="jay-pro" src={shobhaYatra} alt="" />
+              <h4>Shobha Yatra</h4>
+            </div>
+            <div className="jay-pro-content">
+              <div className="jay-pro-content-details">
+                <div className="type-name">
+                  <h4>
+                    Kerala Flood <br />
+                    Response
+                  </h4>
+                  <p>
+                    Rapid aid to the destruction and loss caused by the flooding
+                    and landslides in Idukki and Wayanad.
+                  </p>
+                  <Link className="read-more">
+                    <span>Read More</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="jay-pro-type-item">
+            <div className="jay-pro-img">
+              <img className="jay-pro" src={bhashan} alt="" />
+              <h4>Child Champion</h4>
+            </div>
+            <div className="jay-pro-content">
+              <div className="jay-pro-content-details">
+                <div className="type-name">
+                  <h4>
+                    Kerala Flood <br />
+                    Response
+                  </h4>
+                  <p>
+                    Rapid aid to the destruction and loss caused by the flooding
+                    and landslides in Idukki and Wayanad.
+                  </p>
+                  <Link className="read-more">
+                    <span>Read More</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="jay-pro-type-item">
+            <div className="jay-pro-img">
+              <img className="jay-pro" src={shobhaYatra} alt="" />
+              <h4>Procession</h4>
+            </div>
+            <div className="jay-pro-content">
+              <div className="jay-pro-content-details">
+                <div className="type-name">
+                  <h4>
+                    Kerala Flood <br />
+                    Response
+                  </h4>
+                  <p>
+                    Rapid aid to the destruction and loss caused by the flooding
+                    and landslides in Idukki and Wayanad.
+                  </p>
+                  <Link className="read-more">
+                    <span>Read More</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="jay-pro-type-item">
+            <div className="jay-pro-img">
+              <img className="jay-pro" src={shobhaYatra} alt="" />
+              <h4>Procession</h4>
+            </div>
+            <div className="jay-pro-content">
+              <div className="jay-pro-content-details">
+                <div className="type-name">
+                  <h4>
+                    Kerala Flood <br />
+                    Response
+                  </h4>
+                  <p>
+                    Rapid aid to the destruction and loss caused by the flooding
+                    and landslides in Idukki and Wayanad.
+                  </p>
+                  <Link className="read-more">
+                    <span>Read More</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* -----------------------------------------Jayanti Gallery-------------------------------------------------- */}
+      <div className="jayanti-Gallery">
+        <div className="jaya-progr-heading">
+          <h2>Gallery</h2>
         </div>
       </div>
     </div>
