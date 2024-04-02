4091444d7c0db366c58390d40d3454b171a556f2
SahilPawar10 <pawarsp959@gmail.com>
2024-04-02 11:10:28 +0530

    team-table

--- src/AdminLayout/adminHome/AdminHome.jsx
+++ src/AdminLayout/adminHome/AdminHome.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import LayoutAdmin from "../Layout2/LayoutAdmin";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function AdminHome() {
-  return <div>AdminHome</div>;
+  return (
+    <div>
+      {/* search bar  */}
+      <div className="search-bar">
+        <input type="text" placeholder="Search Here" />
+      </div>
+      {/* Table */}
+      <div className="team-table">
+        <table>
+          <thead>
+            <tr>
+              <th className="teamtable-heading">Sr.no</th>
+              <th className="teamtable-heading">Name</th>
+              <th className="teamtable-heading">Number</th>
+              <th className="teamtable-heading">Email</th>
+              <th className="teamtable-heading">Gender</th>
+              <th className="teamtable-heading">Picture</th>
+              <th className="teamtable-heading">Address</th>
+              <th className="teamtable-heading">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Sahil Pawar</td>
+              <td>9764804327</td>
+              <td>sahilpawar@gmail.com</td>
+              <td>Male</td>
+              <td>Picture</td>
+              <td>At post Ambrag tal-patan</td>
+              <td>
+                <MoreVertIcon sx={{ transform: "rotate(0deg)" }} />
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Sahil Pawar</td>
+              <td>9764804327</td>
+              <td>sahilpawar@gmail.com</td>
+              <td>Male</td>
+              <td>Picture</td>
+              <td>At post Ambrag tal-patan</td>
+              <td>
+                <MoreVertIcon sx={{ transform: "rotate(0deg)" }} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default LayoutAdmin(AdminHome, "admin-home");
